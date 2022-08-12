1f6439c39181ca13c2e50f1289f7f67cb84f71d3
Shahbaz Alam <shahbaz.alam@synectiks.com>
2022-08-12 11:21:34 +0530

    deploy 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "demodeploy" {
 
   account_customizations_name = "sandbox"
 }
+
+module "demodeploy1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "salmankhann01234567@gmail.com"
+    AccountName               = "sandbox-aft-new1"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
+    SSOUserFirstName          = "John"
+    SSOUserLastName           = "Demo"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Admin"
+    change_reason       = "Test deployment1"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
